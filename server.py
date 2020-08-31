c3c2c3878db688b1cfd30bfaa62ce3cdff69a9c4
Cazeho <58745332+Cazeho@users.noreply.github.com>
2020-08-31 19:22:42 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,11 @@
 import http.server
+import webbrowser
+from threading import Timer
+import os
+
+def open_browser():
+    chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+    webbrowser.get(chrome_path).open_new('http://localhost:9000/inter.py')
 
 port = 9000
 address = ("",port)
@@ -11,4 +18,7 @@ handler.cgi_directories = ["/"]
 httpd = server(address, handler)
 
 print("Server demarre", port)
+if __name__ == "__main__":
+    Timer(1, open_browser).start()
+
 httpd.serve_forever()
